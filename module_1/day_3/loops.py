97b6938033d6fa5dff2dfac757324897948f1c7a
AayuStha <aayustha21@gmail.com>
2024-01-05 08:56:34 +0545

    prime number

--- module_1/day_3/loops.py
+++ module_1/day_3/loops.py
@@ -3,15 +3,14 @@
 
 #prime number
 
-num = int (input('Enter a number:'))
-
+num = int(input('Enter a number:'))
 count = 0
 
-for i in range (1,num+1):
+for i in range (1 , num+1):
     if num % i == 0:
         count = count+1
 
-if count ==2 :
+if count == 2:
     print("Prime number")
 else:
     print("Not prime")
