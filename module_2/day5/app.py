d4a66576234cd703e66c10396f173475350befcb
AayuStha <aayustha21@gmail.com>
2024-02-25 14:06:49 +0545

    module_2

---
+++ module_2/day5/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("Hello world!")
+st.image("https://www.aayushrestha.com/images/Aayush.JPG")
+st.image("https://static.vecteezy.com/system/resources/thumbnails/025/181/412/small/picture-a-captivating-scene-of-a-tranquil-lake-at-sunset-ai-generative-photo.jpg")
